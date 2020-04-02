0675fa347d777541a11810ad4d2f5c6d630a4502
wongro2 <roy.wong@oocl.com>
2020-04-02 22:21:25 +0800

    finished section 3

--- practice/section-3/practice-4.js
+++ practice/section-3/practice-4.js
@@ -1,5 +1,67 @@
 'use strict';
 
 function createUpdatedCollection(collectionA, objectB) {
-  return 'Implement the practice require, and begin changing code in this row';
+	var convertCol=[];
+	var CollectionC =[];
+	var minus = 1;
+	var count = 0;
+	var x;
+	var y;
+	var temp=[];
+	var result=[];
+
+	//for(x in collectionA){
+	//	if(!temp.includes(collectionA[x])){
+	//		temp[count] = collectionA[x];
+	//		count++;
+	//	}
+	//}
+	for (x in collectionA) {
+		if (collectionA[x].length==1){
+			convertCol.push(collectionA[x]);
+		}	else {
+			var temp = collectionA[x].split(collectionA[x].charAt(1))
+			var tempcount = parseInt(temp[1]);
+			for (var i = 0; i<tempcount; i++){
+				convertCol.push(temp[0]);
+			}
+		}
+	}
+	for(x in convertCol){
+		if(!temp.includes(convertCol[x])){
+			temp[count] = convertCol[x];
+			count++;
+		}
+	}
+
+	for(x in temp){
+		count=0;
+		for(y in convertCol){
+			if (temp[x]===convertCol[y]){
+				count++
+			}
+		}
+		CollectionC[x]= {key: temp[x], count: count};
+	}
+
+
+	for (var x in CollectionC){
+		for (var y in objectB.value){
+			if (CollectionC[x].key===objectB.value[y]){
+				CollectionC[x].count -= minus;
+				minus ++;
+				if (minus>3){
+					minus=1;
+				}
+			}
+		}
+	}
+
+
+
+
+
+
+ return CollectionC;
+  //return 'Implement the practice require, and begin changing code in this row';
 }
